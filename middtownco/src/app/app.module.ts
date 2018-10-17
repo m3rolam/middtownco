b43bb8838c0f5900c00ce1e8560fc1a455819baa
Michael Merola <michael.merola@nextcentury.com>
2018-10-17 13:33:52 -0400

    Added router and pages

--- middtownco/src/app/app.module.ts
+++ middtownco/src/app/app.module.ts
@@ -3,20 +3,55 @@ import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { FeaturedComponent } from './featured/featured.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { CarouselComponent } from './Components/carousel/carousel.component';
+import { FeaturedComponent } from './Components/featured/featured.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ShopComponent } from './Components/shop/shop.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { ProductsComponent } from './Components/products/products.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'shop',
+    component: ShopComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     CarouselComponent,
-    FeaturedComponent
+    FeaturedComponent,
+    ShopComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
